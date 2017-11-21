92da1e56670bbf61b86b9c75ba2c0f11e8b9eba6
mickael <mickael@10.106.128.24>
2017-11-21 10:02:14 +0100

    stockage des données

--- src/model/Model.java
+++ src/model/Model.java
@@ -4,11 +4,40 @@ package model;
 import java.util.*;
 
 public class Model {
-	private int rosée ;
-	private int température ;
-	private int humidité ;
-	private int consigne ;
+	private double dew ;
+	private double temperature ;
+	private double humidity ;
+	private double order ;
 	ArrayList listeTemp = new ArrayList();
-	
+	public double getDew() {
+		return dew;
+	}
+	public void setDew(double dew) {
+		this.dew = dew;
+	}
+	public double getTemperature() {
+		return temperature;
+	}
+	public void setTemperature(double temperature) {
+		this.temperature = temperature;
+	}
+	public double getHumidity() {
+		return humidity;
+	}
+	public void setHumidity(double humidity) {
+		this.humidity = humidity;
+	}
+	public double getOrder() {
+		return order;
+	}
+	public void setOrder(double order) {
+		this.order = order;
+	}
+	public ArrayList getListeTemp() {
+		return listeTemp;
+	}
+	public void setListeTemp(ArrayList listeTemp) {
+		this.listeTemp = listeTemp;
+	}
 	
 }

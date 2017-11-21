57c38d8ebc98f36b37023d95a5e5a61da9fcb293
baelishSir <sylvain.lerest@viacesi.fr>
2017-11-21 13:57:34 +0100

    Ajout du lancement du modele

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -1,9 +1,11 @@
 package controller;
 import model.Model;
+import model.ConnectionArduino;
 import view.View;
 public class Controller {
 	private final View  view;
 	private final Model model;
+
 
 	public Controller(final View view, final Model model){
 		this.view = view;
@@ -12,5 +14,18 @@ public class Controller {
 
 	public void launch(){	
 		this.view.setVisible(true);
+		
+		ConnectionArduino connectionArduino = new ConnectionArduino();
+		connectionArduino.initialize();
+		Thread t=new Thread() {
+			public void run() {
+				try {
+					Thread.sleep(1000000);
+					}catch (InterruptedException ie) {}
+			}
+		};
+		t.start();
+		System.out.println(" Started ");
+		
 	}
-}//
+}

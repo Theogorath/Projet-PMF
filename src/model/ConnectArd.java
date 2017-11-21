fb2ff0c9bf6d7df6f6b3cb88ce3ef152b7b8c4ea
mickael <mickael@10.106.128.24>
2017-11-21 08:42:46 +0100

    connectArd

--- src/model/ConnectArd.java
+++ src/model/ConnectArd.java
@@ -1,5 +1,17 @@
 package model;
 
-public class ConnectArd {
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import gnu.io.CommPortIdentifier;
+import gnu.io.SerialPort;
+import gnu.io.SerialPortEvent;
+import gnu.io.SerialPortEventListener;
+import java.util.Enumeration;
 
+
+public class ConnectArd implements SerialPortEventListener{
+
+	
+	
 }
--- src/model/Model.java
+++ src/model/Model.java
@@ -1,8 +1,14 @@
+
 package model;
+
+import java.util.*;
 
 public class Model {
 	private int rosée ;
 	private int température ;
 	private int humidité ;
+	private int consigne ;
+	ArrayList listeTemp = new ArrayList();
+	
 	
 }
